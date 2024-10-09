00769ec924d409d0e100b1c79ba9c8d6ce15cc2a
cruttam7 <uttamd@students.federation.edu.au>
2024-10-09 11:09:58 +1030

    drftj

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -4,8 +4,12 @@ const adminSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, default: 'admin' },  // Setting the default role as 'admin'
-  createdAt: { type: Date, default: Date.now }
+  role: { type: String, default: 'admin' },  // Default role as 'admin'
+  createdAt: { type: Date, default: Date.now },
+
+  // Add resetPasswordToken and resetPasswordExpires for password reset functionality
+  resetPasswordToken: { type: String },
+  resetPasswordExpires: { type: Date }
 });
 
 const Admin = mongoose.model('Admin', adminSchema);
